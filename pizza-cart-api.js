f882cc1096ca38bfcee6d89d1d6ca3010ea91bef
TanakaCalebTakarinda <calebtakks@gmail.com>
2022-08-11 21:53:17 +0200

    working

--- pizza-cart-api.js
+++ pizza-cart-api.js
@@ -5,29 +5,57 @@ document.addEventListener('alpine:init', () => {
                 //call API to get all Pizza's
                 //set his.pizzas
                 axios
-                .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+                    .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
                     .then((result) => {
                         //console.log(result.data)
                         this.pizzas = result.data.pizzas
-                    });
-            },
-            createCart(){
-                ///api/pizza-cart/create
-                axios
-                .get('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username'+ this.username)
+                    })
+                    .then(() => {
+                        return this.createCart();
+
+                    })
                     .then((result) => {
-                        //console.log(result.data)
-                       // this.pizzas = result.data.pizzas
-                       this.cartId = result.data.cart_code
+                        this.cartId = result.data.cart_code;
+
+                    });
+
+
+            },
+            createCart() {
+                ///api/pizza-cart/create
+                return axios.get('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username' + this.username)
+            },
+
+            showCart() {
+                const url =`https://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get`;
+                axios
+                    .get(url)
+                    .then((result) => {
+                        this.cart = result.data;
                     });
             },
-            message: 'Eating Pixxa',
-            username: '',
-            cartId:'',
+            pizzaImage(pizza){
+                return `/img/${pizza.size}.png`
+            },
+
+            message: 'Eating Pizza',
+            username: 'TanakaCalebTakarinda',
+            cartId: '',
             pizzas: [],
-            add(pizza){
+            cart: { total: 0 },
+
+            add(pizza) {
                 //need a cart Id so that i can add pizza to cart
-                
+                const params = {
+                    cart_code: this.cartId,
+                    pizza_id: pizza.id
+                }
+                axios
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/add', params)
+                    .then(() => {
+                        this.message = "Pizza added to the cart"
+                        this.showCart();
+                    })
             }
         }
     })
